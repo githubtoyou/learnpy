5ffa2a61e36f6ebd331fa469b373a5dd668a343b
hhq <pusimple@126.com>
2021-04-29 10:43:26 +0800

    1

---
+++ error-test/debug.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+import functools
+import sys
+import types,logging,pdb
+
+# def foo(s):
+#     n = int(s)
+#     # print('>>> n = %d ' % n)
+#     assert n != 0 ,'n is zero !'
+#     return 10 / n
+# def main():
+#     foo('0')
+# main()
+
+# logging.basicConfig(level=logging.INFO)
+# s = '0'
+# n = int(s)
+# logging.info('n = %d' % n)
+# print(10 / n)
+
+s = '0'
+n = int(s)
+pdb.set_trace()
+print(10 / n)
+
