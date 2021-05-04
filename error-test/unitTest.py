03832ed7c0b60d1dd1a0f1d6bf592953d18a3553
hhq <pusimple@126.com>
2021-05-04 14:13:52 +0800

    1

--- error-test/unitTest.py
+++ error-test/unitTest.py
@@ -1,13 +1,104 @@
 # -*- coding: utf-8 -*-
 import os,types,logging,pdb,sys,functools
+import unittest
 
-class Dict(dict):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-    def __getattr__(self, key):
+# class Dict(dict):
+#     def __init__(self, **kw):
+#         super().__init__(**kw)
+#     def __getattr__(self, key):
+#         try:
+#             return self[key]
+#         except KeyError:
+#             raise AttributeError(r"'Dict' object has no attribute ' %s'" % key)
+#     def __setattr__(self, key, value):
+#         self[key] = value
+# # d= Dict(a='key1',b='value')
+# # print(d.a,d.b)
+#
+# class TestDict(unittest.TestCase):
+#
+#     def test_init(self):
+#         d = Dict(a=1, b='test')
+#         self.assertEqual(d.a, 1)
+#         self.assertEqual(d.b, 'test')
+#         self.assertTrue(isinstance(d, dict))
+#
+#     def test_key(self):
+#         d = Dict()
+#         d['key'] = 'value'
+#         self.assertEqual(d.key, 'value')
+#
+#     def test_attr(self):
+#         d = Dict()
+#         d.key = 'value'
+#         self.assertTrue('key' in d)
+#         self.assertEqual(d['key'], 'value')
+#
+#     def test_keyerror(self):
+#         d = Dict()
+#         with self.assertRaises(KeyError):
+#             value = d['empty']
+#
+#     def test_attrerror(self):
+#         d = Dict()
+#         with self.assertRaises(AttributeError):
+#             value = d.empty
+#
+#     def setUp(self):
+#         print('setUp...')
+#
+#     def tearDown(self):
+#         print('tearDown...')
+#
+# if __name__ == '__main__':
+#     unittest.main()
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def get_grade(self):
         try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Dict' object has no attribute ' %s'" % key)
-    def __setattr__(self, key, value):
-        self[key] = value
+            if self.score >= 60 and self.score < 80:
+                return 'B'
+            elif self.score >= 80 and self.score <=100:
+                return 'A'
+            elif self.score < 60 and self.score >= 0 :
+                return 'C'
+            else :
+                raise ValueError
+        except ValueError as e:
+            print("Invaild value ######### %s " %e)
+            raise ValueError
+
+class TestStudent(unittest.TestCase):
+
+    def test_80_to_100(self):
+        s1 = Student('Bart', 80)
+        s2 = Student('Lisa', 100)
+        self.assertEqual(s1.get_grade(), 'A')
+        self.assertEqual(s2.get_grade(), 'A')
+
+    def test_60_to_80(self):
+        s1 = Student('Bart', 60)
+        s2 = Student('Lisa', 79)
+        self.assertEqual(s1.get_grade(), 'B')
+        self.assertEqual(s2.get_grade(), 'B')
+
+    def test_0_to_60(self):
+        s1 = Student('Bart', 0)
+        s2 = Student('Lisa', 59)
+        self.assertEqual(s1.get_grade(), 'C')
+        self.assertEqual(s2.get_grade(), 'C')
+
+    def test_invalid(self):
+        s1 = Student('Bart', -1)
+        s2 = Student('Lisa', 101)
+        with self.assertRaises(ValueError):
+            s1.get_grade()
+        with self.assertRaises(ValueError):
+            s2.get_grade()
+
+if __name__ == '__main__':
+    unittest.main()
+
